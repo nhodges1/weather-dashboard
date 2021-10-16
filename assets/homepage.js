32113eff4b6b13b265c94ed9a970a22aef0998fe
nhodges <hodges27@outlook.com>
2021-10-16 01:20:13 -0400

    api key updated

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,24 +1,21 @@
-var formInput = document.querySelector('.formInput')
+var formInput = document.querySelector('.formInput');
+var button = document.querySelector('.btn');
 
 var citySearch = function() {
-    // format the weather api url
-    var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + formInput.value + "&appid=628288e19078b9bbe2d723b6f7ef2029"
     
-    fetch(apiUrl)
-        .then(function(response) {
-            // request was successful
-            if (response.ok) {
-                response.json().then(function(data) {
-                    
-                })
-            } else {
-                alert("Error: Github User Not Found");
-            }
-        })
-        .catch(function(error) {
-            // Notice this '.catch()' getting chained onto the end of the '.then()' method
-            alert("Unable to connect to Github");
-        });
+    button.addEventListener('click', function(){
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+formInput+'&appid=75a3cc3d216cd996d36d719ba932debd')
+            .then(function(response) {
+                // request was successful
+                if (response.ok) {
+                    response.json().then(function(data) {
+                        
+                    })
+                } else {
+                    alert("Error: City Not Found");
+                }
+            })
+    })
 };
 
 citySearch();
