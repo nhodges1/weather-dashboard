b622c494452786b23a45b9795b0ddefa267f0d15
nhodges <hodges27@outlook.com>
2022-02-07 17:34:21 -0500

    search histroy

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,28 @@
 var searchHistory = [];
 var weatherApi= 'https://api.openweathermap.org';
 var weatherApiKey= 'b8adb13d1cae612a2fdba72f3b012bf6';
+
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
 var todayContainer = document.querySelector('#today');
+var forecastContainer = doucment.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history');
+
+// timezone plugins
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+//search history display
+function renderSearchHistory() {
+    searchHistoryContainer.innerHTML = ''; 
+
+    for (var i = searchHistory.length - 1; i >= 0; i--) {
+        var btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('aria-controls', 'today forecast');
+        btn.classList.add('history-btn', 'btn-history');
+    }
+}
 
 var citySearch = function() {
     
