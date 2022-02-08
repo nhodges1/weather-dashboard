2074fd620fe801ea54c63083580b9d4b154ec609
nhodges <hodges27@outlook.com>
2022-02-08 17:46:39 -0500

    local storage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -12,17 +12,46 @@ var searchHistoryContainer = document.querySelector('#history');
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
-//search history display
+// search history display
 function renderSearchHistory() {
     searchHistoryContainer.innerHTML = ''; 
 
+    // start at the end of history array
     for (var i = searchHistory.length - 1; i >= 0; i--) {
         var btn = document.createElement('button');
         btn.setAttribute('type', 'button');
         btn.setAttribute('aria-controls', 'today forecast');
         btn.classList.add('history-btn', 'btn-history');
+
+        // data-search allows access to city name
+        btn.setAttribute('data-search', searchHistory[i]);
+        btn.textContent = searchHistory[i];
+        searchHistoryContainer.append(btn);
     }
 }
+
+// update history in local storage
+function appendToHistory(search) {
+    // if there is no search term return the function
+    if (searchHistory.indexOf(search) !== -1) {
+        return;
+    }
+    searchHistory.push(search);
+
+    localStorage.setItem('search-history', JSON.stringify(searchHistory));
+    renderSearchHistory();
+}
+
+// search history from local storage
+function initSearchHistory() {
+    var storedHistory = localStorage.getItem('search-history');
+    if (storedHistory) {
+        searchHistory = JSON.parse(storedHistory);
+    }
+    renderSearchHistory();
+}
+
+
 
 var citySearch = function() {
     
